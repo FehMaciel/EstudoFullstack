e80f972a11bb4a15c4537f4b313ca5449280f2c3
Felippe Maciel <58644600+FehMaciel@users.noreply.github.com>
2023-04-30 19:49:55 -0300

    Create condicionasjs.js

---
+++ condicionasjs.js
@@ -0,0 +1,8 @@
+const idade = 22;
+if (idade < 18) {
+  console.log("Você não pode dirigir, e nem beber.");
+} else if (idade >= 18 && idade < 21) {
+  console.log("Você pode dirigir, mas não pode beber.");
+} else {
+  console.log("Você pode dirigir, e beber, mas não a mesmo tempo.");
+}
