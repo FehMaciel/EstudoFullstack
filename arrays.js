a6fa6056add40cff6756a23a6cedd3dcbf500cd2
Felippe Maciel <58644600+FehMaciel@users.noreply.github.com>
2023-04-30 19:59:55 -0300

    Update arrays.js

--- arrays.js
+++ arrays.js
@@ -1,3 +1,12 @@
 const numeros = [1, 2, 3, 4, 5];
 console.log(numeros[0]); // 1
 console.log(numeros[2]); // 3
+
+numeros.push(6);
+console.log(numeros); // [1, 2, 3, 4, 5, 6]
+
+numeros.pop();
+console.log(numeros); // [1, 2, 3, 4, 5]
+
+numeros.splice(2, 1, 8);
+console.log(numeros); // [1, 2, 8, 4, 5]
