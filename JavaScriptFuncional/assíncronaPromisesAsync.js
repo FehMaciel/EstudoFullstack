e1600920309ed140067dc5bb5a7b1526796ab4d1
Felippe Maciel <58644600+FehMaciel@users.noreply.github.com>
2023-04-30 22:03:19 -0300

    Update assíncronaPromisesAsync.js

--- JavaScriptFuncional/assíncronaPromisesAsync.js
+++ JavaScriptFuncional/assíncronaPromisesAsync.js
@@ -1,17 +1,18 @@
+  // Exemplo usando Promises
 fetch('https://jsonplaceholder.typicode.com/todos/1')
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error(error));
+//"then" para manipular a resposta quando ela estiver pronta.
+.then(response => response.json())
+.then(data => console.log(data))
+.catch(error => console.error(error));
 
-
-  async function getTodo() {
-    try {
-      const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
-      const data = await response.json();
-      console.log(data);
-    } catch (error) {
-      console.error(error);
-    }
-  }
-  getTodo();
-  
+// Exemplo usando Async/await
+async function getTodo() {
+try {
+  const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+  const data = await response.json();
+  console.log(data);
+} catch (error) {
+  console.error(error);
+}
+}
+getTodo();
