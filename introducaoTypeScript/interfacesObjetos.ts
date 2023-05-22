1c0429a5eeace8a48cd4bc2e1752ffedf40afcd6
Felippe Maciel <58644600+FehMaciel@users.noreply.github.com>
2023-05-22 11:21:01 -0300

    Create interfacesObjetos.ts

---
+++ introducaoTypeScript/interfacesObjetos.ts
@@ -0,0 +1,12 @@
+interface Produto{
+    nome: string;
+    preco: number;
+}
+
+const produto: Produto = {
+    nome: "Celular",
+    preco: 2500
+}
+
+console.log(produto.nome);
+console.log(produto.preco);
